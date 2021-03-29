a1fb761cba4a848f72f0195b211304aeebb46056
Cleiton Gaioti <cleiton.s.gaioti@gmail.com>
2021-03-29 09:54:53 -0300

    Trabalho Finalizado

--- teste/main.cpp
+++ teste/main.cpp
@@ -6,6 +6,14 @@
 
 using namespace std;
 
+/*
+ *  Lê os dados de entrada e armazena no vetor de Posições Candidatas,
+ *  cada posição do vetor é um struct Campo, que armazena o id da posição,
+ *  a quantidade de conflitos dela e os pontos conflitantes. Essa função 
+ *  também inicializa e preeenche o vetor de pesos das posições, o peso é 
+ *  dado pelo (nº de conflitos)/(nº de pontos).
+ */
+
 void lerArquivo(string arq){
     FILE* f = fopen(arq.c_str(), "r");
 
@@ -25,15 +33,9 @@ void lerArquivo(string arq){
     fclose(f);
 }
 
-int gerarNumero(int lim_inf, int lim_sup){
-    return(lim_inf + rand() % (lim_sup-lim_inf+1));
-}
-
-void heuConAle(Solucao &s) {
-    for(int j = 0; j < numeroDePontos; j++) {
-        s.posicaoDosPontos[j] = gerarNumero(j*numeroDePosicoesCandidatas + 1, (j+1)*numeroDePosicoesCandidatas);
-    }
-}
+/*
+ *  Atribui a cada ponto a sua posição com o menor peso.
+ */
 
 void heuConGul(Solucao &s) {
 
@@ -51,6 +53,13 @@ void heuConGul(Solucao &s) {
     }
     
 }
+
+/*
+ *  Percorre cada ponto do vetor de posições e verifica se os outros pontos da solução estão
+ *  presentes na sua lista de conflitos, se houver conflitos, marcam os pontos no vetor  de 
+ *  posições conflitantes, no fim da função contabiliza o nº de pontos livres verificando se 
+ *  o valor original da posição do vetor foi alterado.
+ */
 
 void calcularFO(Solucao &s){
     
@@ -84,46 +93,57 @@ void clonarSolucao(Solucao &s1, Solucao &s2){
     } 
 }
 
-void testarDados(Solucao &s, string arq)
-{
+void testarDados(Solucao &s, string arq){
     FILE *f;
-    if(arq.empty())
-        f = stdout;
-    else
+    if(arq.empty()){
+        printf("Numero de pontos %d\n", numeroDePontos);
+        printf("Numero de posicoes candidatas %d\n", numeroDePosicoesCandidatas);
+        printf("Numero de conflitos %d\n", s.numeroDeConflitos);
+        printf("Valor da FO %d\n", s.funcao_objetivo);
+        cout << "Solução completa gerada no arquivo solHeuConGul.txt\n" << endl;
+    } else {
         f = fopen(arq.c_str(), "w");
-    fprintf(f, "Numero de pontos %d\n", numeroDePontos);
-    fprintf(f, "Numero de posicoes candidatas %d\n", numeroDePosicoesCandidatas);
-    fprintf(f, "Numero de conflitos %d\n", s.numeroDeConflitos);
-    fprintf(f,"Valor da FO %d\n", s.funcao_objetivo);
-    for(int i = 0; i < numeroDePontos; i++)
-        fprintf(f, "%d\n", s.posicaoDosPontos[i]);
+        fprintf(f, "Numero de pontos %d\n", numeroDePontos);
+        fprintf(f, "Numero de posicoes candidatas %d\n", numeroDePosicoesCandidatas);
+        fprintf(f, "Numero de conflitos %d\n", s.numeroDeConflitos);
+        fprintf(f, "Valor da FO %d\n", s.funcao_objetivo);
+        fprintf(f, "Posição de cada ponto\n");
+        for(int i = 0; i < numeroDePontos; i++)
+            fprintf(f, "%d\n", s.posicaoDosPontos[i]);
+    }
     if(arq != "")
-        fclose(f);
+    fclose(f);
 }
 
 void lerSolucaoDeArquivo(Solucao &s, string path){
-    FILE* f = fopen(path.c_str(), "r");
+    try {
+        FILE* f = fopen(path.c_str(), "r");
 
-    fscanf(f, "%d", &numeroDePontos);
-    cout << numeroDePontos << endl;
-    fscanf(f, "%d", &numeroDePosicoesCandidatas);
-    cout << numeroDePosicoesCandidatas << endl;
-    fscanf(f, "%d", &s.funcao_objetivo);
-    cout << s.funcao_objetivo << endl;
+        fscanf(f, "%d", &numeroDePontos);
+        fscanf(f, "%d", &numeroDePosicoesCandidatas);
+        fscanf(f, "%d", &s.funcao_objetivo);
 
-    for (int i = 0; i <= numeroDePontos; i++) {
-        fscanf(f, "%d", &s.posicaoDosPontos[i]);
-        cout << s.posicaoDosPontos[i] << endl;
+        for (int i = 0; i < numeroDePontos; i++) {
+            fscanf(f, "%d", &s.posicaoDosPontos[i]);
+        }
+
+        cout << "Solução copiada\n" << endl;
+        fclose(f);
+    } catch (exception e){
+        cout << "Não foi possível abrir o arquivo!\n" << endl;
     }
-
-    fclose(f);
 }
+
+/*
+ *  Roda o método de calcular a FO 1000 vezes e captura o tempo de execução.
+ */
 
 void testarF0(Solucao &s){
     clock_t h;
     double tempo;
-    heuConAle(s);
-    
+
+    heuConGul(s);    
+
     h = clock();
     for (int i = 0; i < 1000; i++) {
         calcularFO(s);
@@ -134,19 +154,9 @@ void testarF0(Solucao &s){
     cout << "Tempo de execução: " << tempo << "s" << endl;
 }
 
-void testarHeuristicaConstrutivaAleatoria(Solucao &s) {
-    clock_t h;
-    double tempo;
-    
-    h = clock();
-    for (int i = 0; i < 1000; i++) {
-        heuConAle(s);
-    }
-    h = clock() - h;
-
-    tempo = (double) h/CLOCKS_PER_SEC;
-    cout << "Tempo de execução: " << tempo << "s" << endl;
-}
+/*
+ *  Roda o método da heurística 1000 vezes e captura o tempo de execução.
+ */
 
 void testarHeuristicaConstrutivaGulosa(Solucao &s) {
     clock_t h;
@@ -155,6 +165,97 @@ void testarHeuristicaConstrutivaGulosa(Solucao &s) {
     h = clock();
     for (int i = 0; i < 1000; i++) {
         heuConGul(s);
+    }
+    h = clock() - h;
+
+    tempo = (double) h/CLOCKS_PER_SEC;
+    cout << "Tempo de execução: " << tempo << "s" << endl;
+}
+
+/*
+ *  Captura o tempo de execução dos métodos da heurística e de calcular a FO 1 vez
+ */
+
+void solucaoInicialGulosa(Solucao &s) {
+    clock_t h;
+    double tempo;
+    h = clock();
+
+    heuConGul(s);
+    calcularFO(s);
+
+    h = clock() - h;
+
+    tempo = (double) h/CLOCKS_PER_SEC;
+    printf("Tempo de execução: %.5lfs\n", tempo);
+    cout << "Valor da FO: " << s.funcao_objetivo << endl;
+}
+
+
+int main(){
+
+    Solucao sol;
+    lerArquivo("i13206p8.txt");
+    //lerSolucaoDeArquivo(sol1, "arq.txt");
+
+    int opcao;
+    string arq;
+
+    do {
+        cout << endl;
+        cout << "1 - SOLUÇÃO INICIAL" << endl;
+        cout << "2 - TESTAR HEURÍSTICA" << endl;
+        cout << "3 - TESTAR FUNÇÃO OBJETIVO" << endl;
+        cout << "4 - IMPRIMIR SOLUÇÃO" << endl;
+        cout << "0 - SAIR" << endl;
+
+        cout << "\nEscolha uma opção: ";
+        cin >> opcao;
+        cout << endl;
+
+        switch (opcao) {
+        case 1:
+            solucaoInicialGulosa(sol);
+            testarDados(sol, "solHeuConGul.txt");
+            break;
+        case 2:
+            testarHeuristicaConstrutivaGulosa(sol);
+            break;
+        case 3:
+            testarF0(sol);
+            break;
+        case 4:
+            testarDados(sol, "");
+            break;
+        case 0:
+            break;
+        default:
+            cout << "OPÇÃO INVÁLIDA!" << endl;
+        }
+
+    }  while(opcao != 0);
+
+    return 0;
+}
+
+/*
+int gerarNumero(int lim_inf, int lim_sup){
+    return(lim_inf + rand() % (lim_sup-lim_inf+1));
+}
+
+void heuConAle(Solucao &s) {
+    for(int j = 0; j < numeroDePontos; j++) {
+        s.posicaoDosPontos[j] = gerarNumero(j*numeroDePosicoesCandidatas + 1, (j+1)*numeroDePosicoesCandidatas);
+    }
+}
+
+void testarHeuristicaConstrutivaAleatoria(Solucao &s) {
+    clock_t h;
+    double tempo;
+    
+    h = clock();
+    for (int i = 0; i < 1000; i++) {
+        heuConAle(s);
     }
     h = clock() - h;
 
@@ -176,70 +277,4 @@ void solucaoInicialAleatoria(Solucao &s){
     printf("Tempo de execução: %.5lfs\n", tempo);
     cout << "Valor da FO: " << s.funcao_objetivo << endl;
 }
-
-void solucaoInicialGulosa(Solucao &s) {
-    clock_t h;
-    double tempo;
-    h = clock();
-
-    heuConGul(s);
-    calcularFO(s);
-
-    h = clock() - h;
-
-    tempo = (double) h/CLOCKS_PER_SEC;
-    printf("Tempo de execução: %.5lfs\n", tempo);
-    cout << "Valor da FO: " << s.funcao_objetivo << endl;
-}
-
-
-int main(){
-
-    Solucao sol1, sol2;
-    lerArquivo("i13206p8.txt");
-    //lerSolucaoDeArquivo(sol1, "arq.txt");
-
-    int opcao;
-
-    do {
-        cout << "1 - SOLUÇÃO INICIAL ALEATÓRIA" << endl;
-        cout << "2 - SOLUÇÃO INICIAL GULOSA" << endl;
-        cout << "3 - TESTAR HEURÍSTICA CONSTRUTIVA ALEATÓRIA" << endl;
-        cout << "4 - TESTAR HEURÍSTICA CONSTRUTIVA GULOSA" << endl;
-        cout << "5 - TESTAR FUNÇÃO OBJETIVO ALEATÓRIA" << endl;
-        cout << "6 - TESTAR FUNÇÃO OBJETIVO GULOSA" << endl;
-        cout << "0 - SAIR" << endl;
-
-        cin >> opcao;
-
-        switch (opcao) {
-        case 1:
-            solucaoInicialAleatoria(sol1);
-            testarDados(sol1, "solHeuConAle.txt");
-            break;
-        case 2:
-            solucaoInicialGulosa(sol2);
-            testarDados(sol2, "solHeuConGul.txt");
-            break;
-        case 3:
-            testarHeuristicaConstrutivaAleatoria(sol1);
-            break;
-        case 4:
-            testarHeuristicaConstrutivaGulosa(sol2);
-            break;
-        case 5:
-            calcularFO(sol1);
-            break;
-        case 6:
-            calcularFO(sol2);
-            break;
-        case 0:
-            break;
-        default:
-            cout << "OPÇÃO INVÁLIDA!" << endl;
-        }
-
-    }  while(opcao != 0);
-
-    return 0;
-}
+*/
